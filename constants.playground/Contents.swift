6c417f1a347df814a76bb40c1c606db8c32fcf97
aydincan77 <aydincan77@gmail.com>
2018-01-28 13:42:43 +0300

    Sabitler

---
+++ constants.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Sabitler - Constants 
+
+/* Sabit değer atama şablonu
+
+let <#name#> = <#value#>
+
+ let : Sabit bir değer olacağını,
+ name : Sabite verilecek ismi,
+ = : Değer atanacağını,
+ value : Değerini ifade eder.
+ */
+
+// "name" isimli bir sabite "Can" değerini atayalım ve print konsolunda yazdıralım
+let name = "Can"
+print(name)
+//veya şöyle de yazabilirsiniz
+name
+
+// "soyadi" isimli bir sabite "Can" değerini atayalım ve daha sonra "Aydın" değeri ile değiştirmeyi deneyelim
+var soyadi = "Can"
+soyadi = "Aydın"
+
+// Sabit değer atamaya örnekler
+let sayi = 10
+print(sayi)
+
+let pi = 3.14
+print(pi)
+
+let kullaniciAdi = "Aydın CAN"
+print(kullaniciAdi)
---
+++ variables.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Değişkenler - Variables
+
+/* Değişken değer atama şablonu
+
+var <#name#> = <#value#>
+
+ var : Değişken bir değer olacağını,
+ name : Değişkene verilecek ismi,
+ = : Değer atanacağını,
+ value : Değerini ifade eder.
+ */
+
+// "yaş" isimli bir değişkene "40" değerini atayalım ve print konsolunda yazdıralım
+var yaş = 40
+print(yaş)
+//veya şöyle de yazabilirsiniz
+yaş
+
+// Sabitleri ve değişkenleri diğer sabitlerden ve değişkenlerden tanımlayabilirsiniz.
+let varsayılanSkor = 100
+var birinciOyuncuSkoru = varsayılanSkor
+var ikinciOyuncuSkoru = varsayılanSkor
+
+print(birinciOyuncuSkoru)
+print(ikinciOyuncuSkoru)
+
+birinciOyuncuSkoru = 200
+print(birinciOyuncuSkoru)
+
+// Değişken değer atamaya örnekler
+var userName = "james"
+print(userName)
+
+var age = 30
+print(age)
+
+var myBoolean = true
+print(myBoolean)
+
+
