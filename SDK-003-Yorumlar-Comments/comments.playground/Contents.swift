eace15c048642f8d3245c42b35ef00af5c1dcace
aydincan77 <aydincan77@gmail.com>
2018-03-03 16:41:00 +0300

    ilk

---
+++ SDK-003-Yorumlar-Comments/comments.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Yorumlar - Comments
+
+// Yorumlar, kodlama yaparken kendinize veya diğer geliştiricilere not veya hatırlatma olarak yazdığınız açıklamalardır.
+// Yorumlar iki şekilde oluşturulabilirler. Birincisi tek satırlı yorum ve ikincisi çok satırlı yorum.
+
+//TEK SATIRLIK YORUMA ÖRNEK OLARAK ŞUNLARI VEREBİLİRİZ;
+
+// Ben bir yorumum! Buraya herhangi bir açıklama yazabilirsin.
+
+// veya
+
+// pi’yi sabit bir değer olarak atıyorum. Kod satırında çağırdığımda π değerini 3.14 olarak kabul et.
+let π = 3.14
+
+// veya
+
+let pi = 3.14 // istersem yorumumu böyle de yapabilirim. Yani kod satırımdan sonra en az bir boşluk bırakır, yorum işaretlerimi koyar ve açıklamamı yaparım
+
+//ÇOK SATIRLI YORUMA ÖRNEK OLARAK ŞUNLARI VEREBİLİRİZ;
+
+/*
+ Bende bir yorum çeşidiyim!
+ Beni kullanarak çok satırlı yorumlar yapabilirsin.
+ */
+
+// veya
+
+/* Pi’nin basamakları sonsuzdur,
+ Bunun yerine ben yakın bir yaklaşım seçtim.*/
+let sabitDeğer = 10
+
+// veya
+
+let kullanıcıAdı = "" /* deneme */
