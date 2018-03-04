702377c6f67244a7fee011b80364ba32a7e70e6b
aydincan77 <aydincan77@gmail.com>
2018-03-04 23:26:43 +0300

    second

--- SKD-005-Strings/strings.playground/Contents.swift
+++ SKD-005-Strings/strings.playground/Contents.swift
@@ -12,3 +12,45 @@ import UIKit
  
 */
 
+
+
+// String bir veri tipi
+var greeting = "Welcome!"
+
+// String bir veri tipi
+var ilkDeğer = "10"
+
+// String bir veri tipi
+var pi = "3.14"
+
+// String bir veri tipi
+var çalışıyor = "True"
+
+
+
+//İki String Veriyi birlikte kullanma
+let isimRosa = "Rosa"
+let isimİleSelamlama = "Merhaba, \(isimRosa)!"
+
+print(isimİleSelamlama)
+
+let selamlama = "Merhaba!"
+let dahaUzunSelamlama = selamlama + " Seni yeniden gördüğüme sevindim!"
+
+print(dahaUzunSelamlama)
+
+let selam = "Merhaba!"
+let isim = "Rosa"
+let verileriBirleştir = selam + isim
+
+print(verileriBirleştir)
+
+
+//String veri tiplerini diğer veri tipleri ile birlikte kullanma
+let fiyat = 2
+let miktar = 3
+let kurabiyeFiyatı = "\(miktar) kurabiyenin fiyatı : $\(fiyat * miktar)."
+
+print(kurabiyeFiyatı)
+
+
