d7a98bea80b4ce37da9f0f7914cb4c63fd29cf95
aydincan77 <aydincan77@gmail.com>
2018-03-04 19:52:58 +0300

    first

---
+++ SKD-011-Operatörler-Atama-Operatörü/atama-opertörleri.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Temel Operatörler
+
+// Atama operatörleri - Assignment Operator
+
+let b = 10; print("b sabit değeri :", b)
+var a = 5; print("a değişken değeri :", a)
+
+a = b
+
+print("a değişken değeri :", a)
+print("b sabit değeri :", b)
+
+// -------------------------------------------------
+
+var (x, y) = (1,2)
+print("x değişkeninin değeri :", x)
+print("x değişkeninin değeri :", y)
+x=3; print("x değişkeninin değeri :", x)
+y=4; print("x değişkeninin değeri :", y)
+
+// -------------------------------------
+
+if x = y {
+    10 // Bu geçerli değildir, çünkü x=y bir değer döndürmez.
+}
+
+
---
+++ SKD-012-Operatörler-Aritmetik-Operatörler/Aritmetik-operatörleri.playground/Contents.swift
@@ -0,0 +1,81 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Aritmrtik operatörler - Arithmetic Operators
+
+/*
+
+
+ İşaret    Fonksiyonu
+ +         İki veya daha fazla değeri Toplar
+ -         İki veya daha fazla değeri Çıkarır.
+ *         İki veya daha fazla değeri Çarpar.
+ /         İki veya daha fazla değeri Böler.
+ %         İki sayıyı böler ve kalan değeri verir.
+ -Değer    Söz konusu değeri negatif ile çarpar.
+ +Değer    Söz konusu değeri pozitif ile çarpar.
+ +=        Söz konusu değeri istenen sayı ile toplar.
+ -=        Söz konusu değeri istenen sayı ile çıkarır.
+ *=        Söz konusu değeri istenen sayı ile çarpar.
+ /=        Söz konusu değeri istenen sayı ile böler.
+
+ 
+ */
+
+// Aritmetik işlemlere örnekler - Dört işlem
+var yirmi = 20
+var on = 10
+
+// Toplama işlemi
+var sonuçArtı = yirmi + on
+
+// Çıkarma işlemi
+var sonuçEksi = yirmi - on
+
+// Çarpma işlemi
+var sonuçÇarpı = yirmi * on
+
+// Bölme işlemi
+var sonuçBölüm = yirmi / on
+
+
+// Kalan operatörü - Remainder Operator
+var sonuçYüzde = yirmi % on
+
+var dokuz = 9
+var dört = 4
+var sonuçKalan = dokuz % dört
+
+// Tekil negatif operatör - Unary Minus Operator, Söz konusu değeri negatif ile çarpar
+var a = 5
+var b = -a       // b = -1 * a ifadesi ile aynı işlemi yapar
+
+
+var c = -5
+var d = -c       // d = -1 * c ifadesi ile aynı işlemi yapar
+
+
+// Tekil Pozitif operatör - Unary plus Operator, söz konusu değeri pozitif ile çarpar
+var e = -5
+var f = +e       // f = +1 * e ifadesi ile aynı işlemi yapar
+
+var g = 5
+var h = +g       // h = +1 * g ifadesi ile aynı işlemi yapar
+
+
+// Birleşik atama operatörleri - Compound Assignment Operators
+var i = 1
+i += 2           // i = i + 2 ifadesi ile aynı işlemi yapar
+
+var j = 2
+j -= 1           // j = j - 1 ifadesi ile aynı işlemi yapar
+
+var k = 3
+k *= 4           // k = k * 4 ifadesi ile aynı işlemi yapar
+
+var l = 24
+l /= 4           // l = l / 4 ifadesi ile aynı işlemi yapar
+
+
+
